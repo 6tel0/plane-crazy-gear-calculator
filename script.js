b3ae2b4849d002ceb08fd34f033a5135ed897319
6tel0 <trevorvallandi825@gmail.com>
2025-06-04 04:07:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -22,9 +22,26 @@ function calculate() {
   document.getElementById('halfAngleResult').textContent = `Half-Angle (b):\n${b.toFixed(4)}°`;
   document.getElementById('radiusResult').textContent = `Radius (r):\n${r.toFixed(6)}`;
   document.getElementById('offsetResult').textContent = `Compressor Offset (1 - r + current offset):\n${offset.toFixed(6)}`;
+
+  // Show explanation
+  document.getElementById('explanationBox').style.display = 'block';
+  document.getElementById('explanationText').innerHTML = `
+    <b>How it works:</b><br>
+    You entered <b>${n}</b> teeth. First, we calculate the angle per tooth:<br>
+    <code>a = 360 / n = ${a.toFixed(4)}</code><br>
+    Then we find the half-angle:<br>
+    <code>b = a / 2 = ${b.toFixed(4)}</code><br><br>
+
+    We use this to get the radius of the gear circle using trigonometry:<br>
+    <code>r = cos(b) / sin(a) = ${r.toFixed(6)}</code><br><br>
+
+    Finally, the compressor offset is:<br>
+    <code>Offset = r - current offset = ${r.toFixed(6)} - ${currentOffset} = ${(r - currentOffset).toFixed(6)}</code><br><br>
+
+    This tells you how much to compress to perfectly align your gear in Plane Crazy.
+  `;
 }
 
 function toRad(deg) {
   return deg * (Math.PI / 180);
 }
-
